66986fd32732cf05926130b64040d64c3a922d89
cham-aayush <aayushchamoli2020@gmail.com>
2025-08-26 21:29:47 +0530

    added cookies and flash

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const path = require('path');
 const methodOverride = require('method-override');
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
-
+const session = require('express-session');
+const flash = require('connect-flash');
 
 const listings = require('./routes/listing.js');
 const reviews = require('./routes/review.js');
@@ -29,9 +30,30 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine("ejs", ejsMate);
 
+const sessionOptions = {
+    secret: 'mysecretkey',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7, // 1 week
+        maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+    },
+};
+
 app.get('/',(req,res)=>{
     res.send('Hi I am root');
 });
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+});
+
 
 app.use('/listings', listings);
 
